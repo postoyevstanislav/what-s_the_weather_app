3650ac7dbcd194d4d73a7199b5134d0b46680136
Stanislav Postoyev <mr.postoyev@gmail.com>
2020-09-24 21:17:55 +0200

    refactor if else statements

--- src/components/weather-status-current/weather-status-current.jsx
+++ src/components/weather-status-current/weather-status-current.jsx
@@ -1,67 +1,26 @@
+// import { render } from '@testing-library/react';
 import React from 'react';
 
 import styles from './weather-status-current.module.css'
 
-
-export const WeatherStatusCurrent = (props) => {
-    if(props.status === 'Clear') {
-        return (
+export const WeatherStatusCurrent = ({status, temp}) => { 
+    const renderData = (imageName) => {
+        return(
             <div className={styles.container}>
-                <img className={styles.weatherImg} src={require("../../img/weather-icons/sun.png")} alt=""/>
-                <p className={styles.temperature}> {props.temp}°</p>
+                <img className={styles.weatherImg} src={require(`../../img/weather-icons/${imageName}.png`)} alt=""/>
+                <p className={styles.temperature}> {temp}°</p>
             </div>
-        )
-    } else if(props.status === 'Rain') {
-        return (
-            <div className={styles.container}>
-                <img className={styles.weatherImg} src={require("../../img/weather-icons/rainy.png")} alt=""/>
-                <p className={styles.temperature}> {props.temp}°</p>
-            </div>
-        )
-    } else if (props.status === 'Snow') {
-        return (
-            <div className={styles.container}>
-                <img className={styles.weatherImg} src={require("../../img/weather-icons/snow.png")} alt=""/>
-                <p className={styles.temperature}> {props.temp} °</p>
-            </div>
-        )
-    } else if (props.status === 'Extreme') {
-        return (
-            <div className={styles.container}>
-                <img className={styles.weatherImg} src={require("../../img/weather-icons/thunder.png")} alt=""/>
-                <p className={styles.temperature}> {props.temp}°</p>
-            </div>
-        )
-    } else {
-        return (
-            <div className={styles.container}>
-                <img className={styles.weatherImg} src={require("../../img/weather-icons/cloud.png")} alt=""/>
-                <p className={styles.temperature}> {props.temp}°</p>
-            </div>
-        )
-    }
-
-    
-                
-}
-
-// const WeatherIcon = {
-//         'Clear': 'sun',
-//         'Rain': 'rainy',
-//         'Snow': 'snow',
-//         'Extreme': 'thunder',
-//         'Clouds': 'cloud'
-//     }
-// export const WeatherStatusCurrent = ({status, temp}) => {
-    
-//         if(status) {
-
-//         return(
-//                 <div className={styles.container}>
-//                     <img className={styles.weatherImg} src={require(`../../img/weather-icons/${WeatherIcon[status]}.png`)} alt=""/>
-//                     <p className={styles.temperature}> {temp}°</p>
-//                 </div>
-//             )
-//         }
+        )}
+        switch(status) {
+                case('Clear'):
+                    return renderData('sun')   
+                case ('Rain'):
+                    return renderData('rainy')   
+                case  ('Snow'):
+                    return renderData('snow')       
+                case ('Extreme'):
+                    return renderData('thunder')    
+                default: return renderData('cloud')
+            }
         
-// }
+            }
